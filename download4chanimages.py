e09b6bace67d70e344a44e89fec856fe434757f0
Kyluke <kyluke.m@gmail.com>
2015-11-08 09:03:43 +0200

    Added the ability to define the element and class upfront

--- download4chanimages.py
+++ download4chanimages.py
@@ -6,7 +6,12 @@ from bs4 import BeautifulSoup
 
 url = input("thread to monitor: ")
 newpath = input("save folder: ")
+element = input("which element are you monitoring?: ")
+reference_class = input("what is the class of the element?: ")
 monitor = input("monitor? [yes/no]: ")
+
+amount = 0
+index = 0
 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
@@ -17,15 +22,21 @@ def find_instances(a,f):
             return 1
 
 def save():
+    global amount
+    global index
     page = BeautifulSoup(urllib.request.urlopen(url),'html.parser')
+    for a in page.find_all(element, {'class': reference_class}):
+        amount = amount + 1
     f = []
     for (dirpath, dirnames, filenames) in walk(newpath):
         f.extend(filenames)
         break
-    for a in page.find_all('a', {'class': 'fileThumb'}):
+    for a in page.find_all(element, {'class': reference_class}):
         if find_instances(a,f) != 1:
             urllib.request.urlretrieve("http:"+a['href'], newpath+"\\"+a['href'][-17:])
-            print("Found the URL:", a['href'])
+            index = index + 1
+            print("saved the image:", a['href'], "[",index,"/",amount,"]")
+            amount = 0
             return 1
 
 if monitor == "yes":
@@ -33,6 +44,6 @@ if monitor == "yes":
         if save() != 1:
             print("nothing new yet")
             time.sleep(5)
+
 else:
     save()
-
